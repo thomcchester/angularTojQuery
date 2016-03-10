7dafe88723bb1bbcf97982d0d6eedcb29df73ea2
Thomas Chester <thomcchester@gmail.com>
2016-03-10 08:59:33 -0600

    third

--- client/app.js
+++ client/app.js
@@ -13,27 +13,14 @@ $(document).ready(function(){
     addACat(val);
   });
 
-
-  $.ajax({
-    type: "GET",
-    url: "/cats",
-    success: function(cats){
-      console.log("something");
-      // for (i=0; i<cats.length;i++){
-      //   $(".cats").append("<p>"+cats[i].name+"</p>");
-      //}
-    }
-  });
 });
 
 function addACat(cat){
-  console.log(cat);
-  $('.cats').append("<p>"+cat.name+"</p>");
   $.ajax({
     type: "POST",
     url: "/addCat",
     data: cat,
-    success: function(cats){
+    success: function(cat){
       $.ajax({
         type:"GET",
         url:"/cats",
@@ -47,45 +34,3 @@ function addACat(cat){
 
   });
 }
-
-    // success: function(cats){
-    //   $.ajax({
-    //     type: "GET",
-    //     url: "/cats",
-    //     success: function(cats){
-    //       for (i=0; i<cats.length;i++){
-    //         $(".cats").append("<p>"+cats[i].name+"</p>");
-    //       }
-    //     }
-
-
-
-
-
-        // var app = angular.module('app', []);
-        //
-        // app.controller("IndexController", ['$scope', '$http', function($scope, $http){
-        //   $scope.cat = {};
-        //   $scope.cats = [];
-        //
-        //   console.log("scope.cat=", $scope.cat);
-        //   var fetchCats = function() {
-        //     return $http.get('/cats').then(function(response){
-        //       if(response.status !== 200){
-        // //
-        // //       }
-        //
-        //
-        //       $scope.cat = {};
-        //       $scope.cats = response.data;
-        //       return response.data;
-        //     });
-        //   };
-        //
-        //   $scope.add = function(cat){
-        //     return $http.post('/add', cat).then(fetchCats);
-        //   };
-        //
-        //   fetchCats();
-        //
-        // }]);
